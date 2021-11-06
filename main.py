c5429c3d92d9e9f2fb74e073e4ce08a685f4640a
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2021-11-06 15:01:56 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import linenotif
+import requests
+
+reps = [
+    "table",
+    "hogwarts",
+    "icy-bot"
+]
+
+a = []
+
+noti = linenotif.Notify()
+noti.setKey(os.getenv('LINE_TOKEN'))
+for url in reps:
+    name = url
+    url = "https://" + url + ".aiceaeng.repl.co"
+    res = requests.get(url)
+    if str(res) == '<Response [200]>':
+        a.append(name + ' : OK')
+    else:
+        noti.send(name + ' down!')
+        a.append(name + ' : DOWN')
+s = "\n".join(a)
+print('### Status\n' + s)
