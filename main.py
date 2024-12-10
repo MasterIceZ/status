97c6d42547d09791bdb350facb4827c8bc428811
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2024-12-10 20:56:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,17 @@ a = []
 noti = linenotif.Notify()
 noti.setKey(os.getenv('LINE_TOKEN'))
 for url in reps:
-    name = url
-    url = "https://" + url + ".aiceaeng.repl.co"
-    res = requests.get(url)
-    if str(res) == '<Response [200]>':
-        a.append(name + ' : OK')
-    else:
-        noti.send(name + ' down!')
-        a.append(name + ' : DOWN')
+    try:
+        name = url
+        url = "https://" + url + ".aiceaeng.repl.co"
+        res = requests.get(url)
+        if str(res) == '<Response [200]>':
+            a.append(name + ' : OK')
+        else:
+            # noti.send(name + ' down!')
+            a.append(name + ' : DOWN')
+    except:
+        a.append("MISSING!!!")
 print('### Status\n')
 print('')
 for i in a:
